a252b904ef7de61c123ae9e5b69f44916be09809
shosatojp <me@shosato.jp>
2021-07-22 18:44:55 +0900

    add example

---
+++ data.js
@@ -0,0 +1,49 @@
+const data = {
+    name: {
+        myouji: {
+            kana: "うーばー",
+            kanji: "雨々婆"
+        },
+        namae: {
+            kana: "いーつ",
+            kanji: "何時"
+        }
+    },
+    birthday: {
+        year: 2000,
+        month: 1,
+        day: 1
+    },
+    age: 20,
+    gender: "男",
+    address: {
+        post: "123-4567",
+        kana: "とうきょうと かつしかく",
+        kanji: "東京都 葛飾区"
+    },
+    email: "uber@example.com",
+    phone: "000-0000-0000",
+    picture: "../mypicture.png",
+    sns: {
+        github: "ubereats",
+        twitter: "ubereats",
+        facebook: "ubereats"
+    },
+    history: {
+        school: [
+            {
+                year: "平成30",
+                month: "4",
+                content: "ウーバーイーツ大学 入学"
+            },
+        ],
+        job: []
+    },
+    traveltime: {
+        hours: 1,
+        minutes: 30,
+    },
+    advantages: '',
+    motivation: 'うーばーうーばーうーばーうーばーうーばーうーばーうーばーうーばーうーばーうーばーうーばーうーばーうーばーうーばーうーばーうーばーうーばーうーばーうーばーうーばーうーばーうーばーうーばーうーばーうーばーうーばーうーばー',
+    requests: ''
+}
